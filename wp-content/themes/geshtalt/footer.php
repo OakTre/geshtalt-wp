0b247c512c283db3bd8c90fd4cfcd2216b8f0e1d
NailGalimov <nailgalimov8@gmail.com>
2022-09-05 10:59:54 +0300

    upd

--- wp-content/themes/geshtalt/footer.php
+++ wp-content/themes/geshtalt/footer.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 </main>
 
 <footer class="footer">
@@ -70,77 +69,4 @@
 </body>
 <?php wp_footer(); ?>
 
-=======
-</main>
-
-<footer class="footer">
-    <div class="site-container">
-        <div class="grid-container">
-            <div class="footer__block">
-                <a class="footer__logo" href="/">
-                    <img class="footer__logo-descktop" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-small.svg" alt="Логотип компании">
-                    <img class="footer__logo-mobile" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" alt="Логотип компании">
-                </a>
-            </div>
-            <div class="footer__block">
-                <!-- <a class="footer__legend-link" href="/">Образование</a> -->
-                <a class="footer__legend-link" href="<?php echo get_site_url() . '/event/' ?>">Мероприятия</a>
-                <nav class="footer__nav">
-                    <?php
-                    wp_nav_menu([
-                        'theme_location'  => 'main_menu',
-                        'container'       => null,
-                        'menu_class' => 'menu-list'
-                    ]);
-                    ?>
-                </nav>
-            </div>
-            <div class="footer__block">
-                <ul class="socials footer__socials">
-                    <li class="socials__item">
-                        <a class="socials__link" href="<?php echo $GLOBALS['geshtalt']['wt']; ?>">
-                            <svg class="icon icon-wt socials__icon" width="24" height="24">
-                                <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprites/sprite-mono.svg#wt"></use>
-                            </svg>
-                        </a>
-                    </li>
-                    <li class="socials__item">
-                        <a class="socials__link" href="<?php echo $GLOBALS['geshtalt']['tg']; ?>">
-                            <svg class="icon icon-tg socials__icon" width="24" height="24">
-                                <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprites/sprite-mono.svg#tg"></use>
-                            </svg>
-                        </a>
-                    </li>
-                </ul>
-                <p class="footer__text"><?php echo $GLOBALS['geshtalt']['work_time']; ?></p>
-                <p class="footer__text"><?php echo $GLOBALS['geshtalt']['address']; ?></p>
-                <a class="footer__text _tel" href="tel:<?php echo $GLOBALS['geshtalt']['phone_digits']; ?>"><?php echo $GLOBALS['geshtalt']['phone']; ?></a>
-            </div>
-        </div>
-        <div class="grid-container footer__copyright">
-            <div class="footer__copyright-block"></div>
-            <div class="footer__copyright-block">
-                <a class="footer__copyright-text" href="/">Политика конфиденциальности</a>
-            </div>
-            <div class="footer__copyright-block">
-                <p class="footer__copyright-text">© 2019 Поволжский институт гештальт-терапии</p>
-            </div>
-        </div>
-    </div>
-</footer>
-</div>
-<div class="modal">
-    <div class="modal__container" data-target="modal">
-        <div class="modal__content">
-            <button class="button-reset modal__close-btn modal-close" type="button"><span></span><span></span>
-            </button>
-            <div class="modal__legend">Поздравляем вы открыли модалку!</div>
-        </div>
-    </div>
-</div>
-
-</body>
-<?php wp_footer(); ?>
-
->>>>>>> 080ca8b4fc2331ba3ae275109a10f57bc3141c25
 </html>
--- wp-content/themes/geshtalt/program-card.php
+++ wp-content/themes/geshtalt/program-card.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     $events_id = get_the_ID();
 ?>
@@ -24,31 +23,4 @@
         <button class="button card__btn mod-transparent mod-black"><span class="card__btn-txt">Подробнее</span>
         </button>
     </div>
-=======
-<?php
-    $events_id = get_the_ID();
-?>
-
-
-<div class="card"><span class="card__tag">caption</span>
-    <h3 class="card__heading"><?php the_title(); ?></h3>
-    <div class="card__info">
-        <ul class="card__info-list">
-            <li class="card__info-item">
-                <span class="card__info-legend">Срок обучения</span>
-                <span class="card__info-text"><?php echo carbon_get_post_meta($events_id, 'program_time'); ?></span>
-            </li>
-            <li class="card__info-item">
-                <span class="card__info-legend">Стоимость</span>
-                <span class="card__info-text"><?php echo carbon_get_post_meta($events_id, 'program_cost'); ?></span>
-            </li>
-        </ul>
-        <div class="card__info-descr"><?php the_excerpt(); ?></div>
-    </div>
-    <div class="card__btns">
-        <button class="button card__btn">Записаться</button>
-        <button class="button card__btn mod-transparent mod-black"><span class="card__btn-txt">Подробнее</span>
-        </button>
-    </div>
->>>>>>> 080ca8b4fc2331ba3ae275109a10f57bc3141c25
 </div>
